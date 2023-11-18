2ce668bb1d865f1cb4247383b1cb5d00266ddda0
NTCTudRO <150477889+NTCTudRO@users.noreply.github.com>
2023-11-18 11:32:14 +0200

    Update factura.js

--- factura.js
+++ factura.js
@@ -27,8 +27,8 @@ function functie_factura(){
         document.getElementById("demo_1").appendChild(div)
     });
     suma=suma/1000;
-    let roundedSuma=parseFloat(suma).toFixed(2);
-    document.getElementById("demo_2").innerHTML="Total consume per day is: " + roundedSuma + " kW";
-    document.getElementById("demo_3").innerHTML="Total consume per month is: " + roundedSuma*30 + " kW";
-    document.getElementById("demo_4").innerHTML="Total payment: " + roundedSuma*30*kWh + " lei";
+    let roundedSuma1=parseFloat(suma).toFixed(2), roundedSuma2=parseFloat(suma*30).toFixed(2), roundedSuma3=parseFloat(suma*30*kWh).toFixed(2);
+    document.getElementById("demo_2").innerHTML="Total consume per day is: " + roundedSuma1 + " kW";
+    document.getElementById("demo_3").innerHTML="Total consume per month is: " + roundedSuma2 + " kW";
+    document.getElementById("demo_4").innerHTML="Total payment: " + roundedSuma3 + " lei";
 }
